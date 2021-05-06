41e13471547fdacbdd117124b3fe726a9b509c7a
Vadim Bely <vadim.belyy.02@mail.ru>
2021-05-06 16:32:28 +0300

    updated

--- src/main/java/ru/diplom/FirePandaDelivery/FirePandaDeliveryApplication.java
+++ src/main/java/ru/diplom/FirePandaDelivery/FirePandaDeliveryApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
 @EnableSwagger2
 @SpringBootApplication
 public class FirePandaDeliveryApplication {
 
 	public static void main(String[] args) {
+		System.out.println(Arrays.toString(args));
 		SpringApplication.run(FirePandaDeliveryApplication.class, args);
 	}
 
